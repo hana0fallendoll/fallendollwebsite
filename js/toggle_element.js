76c4382f0a2784c9d66bf3e6b09584dcb1a9f926
hana0fallendoll <102682976+hana0fallendoll@users.noreply.github.com>
2022-03-31 12:56:14 +0700

    site update

---
+++ js/toggle_element.js
@@ -0,0 +1,7 @@
+function toggle_element(element_id, default_display_mode, display_mode_to_toggle_between) {
+    let element = document.getElementById(element_id)
+
+    element.style.display == default_display_mode ?
+        element.style.display = display_mode_to_toggle_between
+        : element.style.display = default_display_mode
+}
